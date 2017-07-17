a090ed546536ee5271df1be820a8970a2a8f8314
jwvg0425 <jwvg0425@naver.com>
2017-07-17 23:56:04 +0900

    add default scripts

---
+++ Assets/Script/Key.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key
+{
+    public static string Up(int UserIdx)
+    {
+        if (UserIdx == 0)
+            return "Up1";
+        else
+            return "Up2";
+    }
+
+    public static string Left(int UserIdx)
+    {
+        if (UserIdx == 0)
+            return "Left1";
+        else
+            return "Left2";
+    }
+
+    public static string Right(int UserIdx)
+    {
+        if (UserIdx == 0)
+            return "Right1";
+        else
+            return "Right2";
+    }
+
+    public static string Down(int UserIdx)
+    {
+        if (UserIdx == 0)
+            return "Down1";
+        else
+            return "Down2";
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool IsPossessed;
+    Rigidbody2D Body;
+    public int Index;
+    public float Hp;
+    public float Speed;
+    public float DashSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (IsPossessed)
+        {
+            Control();
+        }
+        else
+        {
+            AutoMove();
+        }
+	}
+
+    void Control()
+    {
+        var velocity = new Vector2(0.0f, 0.0f);
+
+        if (Input.GetButton(Key.Up(Index)))
+        {
+            velocity.y = 1.0f;
+        }
+
+        if (Input.GetButton(Key.Down(Index)))
+        {
+            velocity.y = -1.0f;
+        }
+
+        if (Input.GetButton(Key.Left(Index)))
+        {
+            velocity.x = -1.0f;
+        }
+
+        if (Input.GetButton(Key.Right(Index)))
+        {
+            velocity.x = 1.0f;
+        }
+
+        if (velocity.x != 0.0f || velocity.y != 0.0f)
+        {
+            velocity.Normalize();
+            velocity *= Speed;
+        }
+
+        Body.velocity = velocity;
+    }
+
+    void AutoMove()
+    {
+    }
+}
