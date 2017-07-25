41735720cddf273aa85707d95fbf752ce6ea0c2a
jwvg0425 <jwvg0425@naver.com>
2017-07-25 23:27:47 +0900

    fix indicator

---
+++ Assets/Script/BottomIndicator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomIndicator : MonoBehaviour
+{	
+	// Update is called once per frame
+	void Update ()
+    {
+        var pos = transform.position;
+        pos.y = -0.032f;
+
+        transform.position = pos;
+		
+	}
+}
