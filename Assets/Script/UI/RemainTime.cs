256a5cc438410b02a24681f40a8cbbe5b06a703c
jwvg0425 <jwvg0425@naver.com>
2017-08-03 21:33:36 +0900

    ui change - bitmap font, background panel added

--- Assets/Script/UI/RemainTime.cs
+++ Assets/Script/UI/RemainTime.cs
@@ -5,12 +5,25 @@ using UnityEngine.UI;
 
 public class RemainTime : MonoBehaviour
 {
+    public bool isMinute;
 	// Update is called once per frame
     void Update()
     {
         int min = (int)GameManager.Instance.RemainGameTime / 60;
         int sec = (int)GameManager.Instance.RemainGameTime % 60;
-        var text = min + ":" + sec;
-        GetComponent<Text>().text = text;
+
+        if (isMinute)
+        {
+            string minStr = min.ToString();
+            GetComponent<Text>().text = minStr;
+        }
+        else
+        {
+            string secStr = sec.ToString();
+            if (sec < 10)
+                secStr = "0" + secStr;
+
+            GetComponent<Text>().text = secStr;
+        }
     }
 }
